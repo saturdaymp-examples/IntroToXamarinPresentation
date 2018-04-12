a1c5c42f50c5197949443891a6e3d179c73c4a53
Chris C <chris.cumming@saturdaymp.com>
2018-04-11 19:24:29 -0600

    HelloWorld from blank app template no changes.

---
+++ Examples/SatudayMP.HelloWorld/SatudayMP.HelloWorld/SatudayMP.HelloWorldPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace SatudayMP.HelloWorld
+{
+    public partial class SatudayMP_HelloWorldPage : ContentPage
+    {
+        public SatudayMP_HelloWorldPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
